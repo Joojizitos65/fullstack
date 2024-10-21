a6088868c4dad2fad336c6e5539ed289bf68c1ab
JOÃO VITOR LUCAS SCHMIDT <joao_vl_schmidt@estudante.sesisenai.org.br>
2024-10-21 09:47:57 -0300

    usuarios

---
+++ mobile/spotifake/app/index.jsx
@@ -0,0 +1,76 @@
+//tela inicial, login
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+      
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      
+      <TextInput
+        style={styles.input}
+        placeholder="Senha"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Entrar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  input: {
+    height: 50,
+    borderColor: '#ddd',
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+  },
+  button: {
+    backgroundColor: '#1DB954', // cor verde do Spotify
+    padding: 15,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
+
+export default LoginScreen;
--- server/index.js
+++ server/index.js
@@ -1,13 +1,26 @@
 import  Express  from "express"
 
 const app = Express()
+app.use(Express.json())
 
 
 app.get('/pegar', function (req, res) {
     res.send('Hello World')
-  })
+})
   app.get('/pegaroutracoisa', function (req, res) {
     res.send('Hello World2')
-  })
+})
+
+app.post('/registro', function (req, res) {
+  try{
+    const {nome, sobrenome, email, senha, datanascimento} = req.body 
+    if( !nome || !sobrenome || !email || !senha || !datanascimento ) {
+      res.send('Todos os campos devem estar preenchidos.')
+    }
+    console.log('Criar usuário')
+  } catch(err){
+
+  }
+})
 
 app.listen(8000)
