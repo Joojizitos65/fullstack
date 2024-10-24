9a26030a4612c0a5e1bf0af1659adc0eb054cda0
JOÃO VITOR LUCAS SCHMIDT <joao_vl_schmidt@estudante.sesisenai.org.br>
2024-10-24 12:02:34 -0300

    token

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import  Express  from "express"
 import { User, criarTabelas } from "./db.js"
 import bcryptjs from "bcryptjs"
+import jwt from "jsonwebtoken"
 
 const app = Express()
 app.use(Express.json())
@@ -30,6 +31,50 @@ app.post('/registro', async function (req, res) {
   } catch(err){
 
   }
+})
+
+app.post('/login', async function (req, res) {
+  //validar informações do login, verfificar se usuário existe, verificar informação de login, token
+  try{
+    const { email, senha } = req.body
+  if (!email || !senha ){
+    res.status(400).send('Todos os campos devem estar preenchidos.')
+    return
+
+  }
+  const usuario = await User.findOne({where: {email: email}})
+  if (!usuario){
+    res.send('Este email não está cadastrado')
+    return
+  }
+  
+  const senhaCorreta = bcryptjs.compareSync(senha, usuario.senha)
+  if(!senhaCorreta){
+    res.send('Senha inválida')
+    return
+  }
+  const token = jwt.sign(
+    //payload
+    //chave de criptografia
+    //tempo de expiração
+    {
+      nome: usuario.nome,
+      email: usuario.email,
+      status: usuario.status
+    },
+    'chavecriptografiasupersegura',
+    { expiresIn: "30d" }
+  )
+  res.send({
+    msg: 'Você foi logado',
+    token: token
+  })
+
+  }catch (erro){
+    console.log(erro)
+    res.status(500).send('Houve um erro.')
+  }
+  
 })
 
 app.listen(8000)
