989ba8de285ff6b1156568f65a4181f2a5d7443e
JOÃO VITOR LUCAS SCHMIDT <joao_vl_schmidt@estudante.sesisenai.org.br>
2024-11-11 08:49:08 -0300

    fazer_rota_autenticacao

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ import { User, criarTabelas } from "./db.js"
 import bcryptjs from "bcryptjs"
 import jwt from "jsonwebtoken"
 import cors from "cors"
-
+import { registro_funcao } from "./controlador/controlador_autenticacao.js"
+import { loginfuncao } from "./controlador/controlador_autenticacao.js"
 
 
 const app = Express()
@@ -13,77 +14,7 @@ app.use(cors())
 
 
 //criarTabelas()
-app.post('/registro', async function (req, res) {
-  try{
-    const {nome, sobrenome, email, senha, datanascimento} = req.body 
-    if( !nome || !sobrenome || !email || !senha || !datanascimento ) {
-      res.send('Todos os campos devem estar preenchidos.')
-    }
-
-    if(await User.findOne({where :{email:email}})){
-      res.status(400).send('Usuário já existe no sistema')
-      return
-    }
-    const senhaSegura = bcryptjs.hashSync(senha, 10)
-
-    const novoUsuario = User.create({
-      nome :nome,
-      sobrenome : sobrenome,
-      email : email,
-      senha : senhaSegura,
-      dataNascimento : datanascimento,
-    })
-    res.status(201).send({msg:
-      'Usuário criado com sucesso.'})
-  } catch(err){
-
-  }
-})
-
-app.post('/login', async function (req, res) {
-  //validar informações do login, verfificar se usuário existe, verificar informação de login, token
-  try{
-    const { email, senha } = req.body
-  if (!email || !senha ){
-    res.status(400).send('Todos os campos devem estar preenchidos para o login.')
-    return
-
-  }
-  const usuario = await User.findOne({where: {email: email}})
-  if (!usuario){
-    res.send('Este email não está cadastrado')
-    return
-  }
-  
-  const senhaCorreta = bcryptjs.compareSync(senha, usuario.senha)
-  if(!senhaCorreta){
-    res.send({msg: 'Senha inválida'})
-    return
-  }
-  const token = jwt.sign(
-    //payload
-    //chave de criptografia
-    //tempo de expiração
-    {
-      nome: usuario.nome,
-      email: usuario.email,
-      status: usuario.status
-    },
-    'chavecriptografiasupersegura',
-    { expiresIn: "30d" }
-  )
-  res.send({
-    msg: 'Você foi logado',
-    token: token
-  })
-
-  }catch (erro){
-    console.log(erro)
-    res.status(500).send('Houve um erro.')
-  }
-  
-})
-
-
+app.post('/registro',registro_funcao )
+app.post('/login', loginfuncao)
 
 app.listen(8000)
